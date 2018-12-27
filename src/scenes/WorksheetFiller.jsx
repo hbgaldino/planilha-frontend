b0cb9b397b72161d91e8e0b63e596c5077d48903
higor.galdino <higor.galdino@accurate.com.br>
2018-12-27 08:19:39 -0200

    scene worksheet filler

---
+++ src/scenes/WorksheetFiller.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WorksheetFiller extends Component {
+    render() {
+        return (
+            <h1>WorksheetFiller</h1>
+        )
+    }
+}
+
+export default WorksheetFiller;
