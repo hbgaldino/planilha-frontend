e890ca6930fa2a212451d581168fffef37f3c075
higor.galdino <higor.galdino@accurate.com.br>
2019-01-03 09:35:02 -0200

    add onchange handler to setstate

--- src/scenes/WorksheetFiller.js
+++ src/scenes/WorksheetFiller.js
@@ -17,10 +17,17 @@ class WorksheetFiller extends Component {
         
        //binding to make 'this' work on method
        this.fillWorkbook = this.fillWorkbook.bind(this);
+       this.handleChange = this.handleChange.bind(this);
     }
 
     fillWorkbook() {
         console.log(this.state);
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
     }
 
     render() {
@@ -30,11 +37,11 @@ class WorksheetFiller extends Component {
                 <GridSubTitle>copyright 2019</GridSubTitle>
                 
                 <Row>
-                    <Input type="text" id="username" label="Usuário"></Input>
-                    <Input type="password" id="password" label="Senha"></Input>
+                    <Input type="text" id="username" label="Usuário" onChange={this.handleChange}></Input>
+                    <Input type="password" id="password" label="Senha" onChange={this.handleChange}></Input>
                     <Column>
-                        <Input type="file" id="file" label="Arquivo"></Input>
-                        <Select label="Mês" id="month" items={MonthsOptions}></Select>
+                        <Input type="file" id="file" label="Arquivo" onChange={this.handleChange}></Input>
+                        <Select label="Mês" id="month" items={MonthsOptions} onChange={this.handleChange}></Select>
                     </Column>
                     <Buttom type="submit" onClick={this.fillWorkbook}>Gerar!</Buttom>
                 </Row>
